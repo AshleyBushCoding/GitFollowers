f71cbbe7387ecf2879ec219a8a4e8f617e6828d7
Laptop\Ashley <Ashley_Bush27@yahoo.com>
2018-10-22 00:03:42 -0500

    - Added the api for the Follower endpoint - Added some data verification - Moved files into api folder.

--- api/FollowerIdApi.py
+++ api/FollowerIdApi.py
@@ -6,7 +6,7 @@
 # readme should be included and checked into github with instructions on 
 # how to execute and test the api
 
-#bonus credit--the api enpoint is publically accessible and fully functional, 
+#bonus credit--the api endpoint is publically accessible and fully functional, 
 # so that team members can execute and test your API at any time.
 
 #done by Ashley Bush under MIT liscense--have fun!
@@ -16,37 +16,36 @@ import requests #for the API calls
 import sys #for command line argument
 import json #for making sure json stays as such!
 
-def get_followers(githubId, maxFollowers = 5):
-    #DO: note--github only allows alphanumeric characters in name, and hyphens.
+def get_followers(githubId, maxNumFollowers = 5):
+    
     #DO: check and see if name is valid
     #add a payload and see if you can get it to work
     #payload = {'key': 'value}
 
     #per github v3 API, per_page allows for making a limit on 
     # returned items (up to 100). Without it, returns every follower 
-    githubApiLink = "https://api.github.com/users/" + githubId + "/followers" + "?per_page=" + str(maxFollowers)
+    githubApiLink = "https://api.github.com/users/" + githubId + "/followers" \
+        + "?per_page=" + str(maxNumFollowers)
+
     response = requests.get(githubApiLink)
     #DO: confirm response was ok before proceeding further
     # example: (response.status_code == requests.code.ok)
     return response.json()
 
-def query_followers(githubId, maxFollowers = 5):
+def query_followers(githubId, maxNumFollowers = 5):
     #note, recursion won't work here because you need to break it at level 3
     #all variables will eventually be stored in response
     #unauthenticated apps can only so may queries per minute and hour 
     # (might be as low as 60 queries per hour). at 3 levels of calls, 
     # 5 each, we're at 5^3, or 125 calls at min. 
     finalList = []
-    level1 = get_followers(githubId, maxFollowers)
+    level1 = get_followers(githubId, maxNumFollowers)
     queryIndex = 0 #search start point for next round
 
     finalList.extend(level1) #level1 is a list    
     stopIndex = len(finalList)
     #DO: CONFIRM Response is not empty before going further!!!!!!!
     #or that it's response is not "exceeded access allowed"
-    testlist = []
-    level2testList = []
-    level3testList = []
 
     # get second level of followers
     for dictionary in finalList[queryIndex : stopIndex]:
@@ -56,8 +55,7 @@ def query_followers(githubId, maxFollowers = 5):
             queryIndex += 1
             userId = dictionary.get('login')
             if(userId is not None):
-                testlist.append(userId)
-                level2 = get_followers(userId, maxFollowers)
+                level2 = get_followers(userId, maxNumFollowers)
                 finalList.extend(level2)
 
     #get third level of followers
@@ -69,39 +67,24 @@ def query_followers(githubId, maxFollowers = 5):
             queryIndex += 1      
             userId = dictionary.get('login')
             if(userId is not None):
-                level2testList.append(userId)
-                level3 = get_followers(userId, maxFollowers)
+                level3 = get_followers(userId, maxNumFollowers)
                 finalList.extend(level3)
 
+    return finalList
 
-    #Testing only--not used in final program
-    stopIndex = len(finalList)
-    #for testing, not needed later
-    for dictionary in finalList[queryIndex : stopIndex]:
-        #DO: confirm that it returns a good value
-        #DO: check that the "dictionary" here isn't a string that says "limit exceeded"
-        if(type(dictionary) is dict):
-            queryIndex += 1      
-            userId = dictionary.get('login')
-            if(userId is not None):
-                level3testList.append(userId)
+def validAlphaNumOrHyphen(userId):
+    for character in userId:
+        if(not character.isalnum() and character is not "-"):
+            return False
+    #else it's good to go
+    return True
 
-
-    print("userIds: " + str(testlist))
-    print("Level 2 userIds: " + str(level2testList))
-    print("Level 3 userIds: " + str(level3testList))
-
-    print ("Full list of users from finalList: ")
-
-    for dictionary in finalList:
-        print(dictionary.get('login'))
-
-    return json.dumps(finalList)
 
 if __name__ == '__main__':
-   # argument needs to be one argument (or 2). Otherwise can't call function
-    #DO: need to validate data and reject if bad
-    if(len(sys.argv) > 2):
-        query_followers(sys.argv[1], sys.argv[2])
-    else:
-        query_followers(sys.argv[1])
+    print("Please use the api to access this code.\n \
+    Please enter userId of the GitHub member you are finding the \n \
+    followers of at the end of the website. You can also optionally add in \n \
+    the number of followers per person referenced (up to 5). \n \
+    Ex: http://127.0.0.1:5000/api/v1/resources/followers?id=jskeet&&maxNumFollowers=2")
+
+      
---
+++ api/api.py
@@ -0,0 +1,45 @@
+# done with help from 
+# https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+
+import flask
+from flask import request, jsonify
+from FollowerIdApi import query_followers, validAlphaNumOrHyphen
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Follower List</h1>\
+    <p>This site is a prototype API to return followers based on a \
+    particular user (up to 3 levels deep).</p>"
+
+# A route to return followers.
+@app.route('/api/v1/resources/followers', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        userId = str(request.args['id'])
+        if(not validAlphaNumOrHyphen(userId)):
+            return "The username entered was not valid (or not present). \
+            The username must be alpha-numeric characters"
+    else:
+        return "Error: No user ID provided. Please specify an id."
+
+    if 'maxFollowers' in request.args:
+        maxFollowers = int(request.args['maxFollowers'])
+    else:
+        maxFollowers = 5
+
+    # Create an empty list for our results
+    results = query_followers(userId, maxFollowers)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+
+app.run()
